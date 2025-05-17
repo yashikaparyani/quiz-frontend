1327fe785163b4039e79179607b4c26b3cc25f11
yashika paryani <yashikaparyani29@gmail.com>
2025-05-17 11:59:24 +0530

    quesions added

--- quiz.js
+++ quiz.js
@@ -23,6 +23,31 @@ let questions = [
       question: "What is the purpose of setTimeout function in JavaScript?",
       options: ["To execute a function immediately", "To execute a function after a delay", "To stop a loop", "To store a variable's value"],
       answer: 1
+  },
+  {
+    question: "What is the result of typeof NaN in JavaScript?",
+    options: ["'number'", "'NaN'", "'undefined'", "'object'"],
+    answer: 0
+  },
+  {
+    question: "Which method converts JSON data to a JavaScript object?",
+    options: ["JSON.stringify()", "JSON.parse()", "JSON.objectify()", "JSON.convert()"],
+    answer: 1
+  },
+  {
+    question: "Which symbol is used for comments in JavaScript?",
+    options: ["//", "/* */", "#", "<!-- -->"],
+    answer: 0
+  },
+  {
+    question: "How do you create a function in JavaScript?",
+    options: ["function myFunction()", "create function myFunction()", "function = myFunction()", "function:myFunction()"],
+    answer: 0
+  },
+  {
+    question: "What will the following code return: Boolean(10 > 9)?",
+    options: ["false", "true", "NaN", "undefined"],
+    answer: 1
   }
 ];
 
